7c12f048e79d2b8b1a125f3c72d890f56cc0d03b
Edanur ANLAYAN <eanlynnea@gmail.com>
2020-08-19 12:28:23 +0300

    Add project files.

---
+++ BarcodeScanner/BarcodeScanner/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace BarcodeScanner
+{
+    public partial class MainPage : ContentPage
+    {
+        ZXingScannerPage scanpage;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void scannerButton_Clicked(object sender, EventArgs e)
+        {
+            scanpage = new ZXingScannerPage();
+            scanpage.OnScanResult += (result) =>
+            {
+                scanpage.IsScanning = false;
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Navigation.PopModalAsync();
+                    DisplayAlert("Scanned Barkod", result.Text, "Ok");
+                });
+            };
+            await Navigation.PushModalAsync(scanpage);
+        }
+    }
+}
